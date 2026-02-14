3be6334387d1af04ab2a812619309f097d622d22
Rishabh Kumar <work.rshbhkmr@gmail.com>
2026-02-14 23:10:04 +0530

    Add files via upload

---
+++ half_adder.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.02.2026 10:13:55
+// Design Name: 
+// Module Name: half_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module half_adder(
+    input a,b,
+    output sum,carry
+    );
+    
+assign sum=a^b;
+assign carry = a&b;
+
+endmodule
